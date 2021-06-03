5d02e84a4a699c3c56aa1bcbde53ba63f0287f24
VladFomenko <cheshirskiy.kot2311@gmail.com>
2021-06-03 15:02:59 +0300

    1. create project file(node_modules, .gitignore, package.json, package-lock.json) 2. new file structure project(create folder src and other) 3. create login_page

---
+++ socialPage/src/js/login_page.js
@@ -0,0 +1,7 @@
+document.getElementById('check').onclick = function () {
+  let login = document.getElementById('login').value;
+  let password = document.getElementById('password').value;
+
+  console.log(login);
+  console.log(password);
+};
