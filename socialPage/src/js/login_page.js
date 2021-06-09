8c1994199ee82d67867766c5153664df45817b5b
VladFomenko <cheshirskiy.kot2311@gmail.com>
2021-06-09 14:10:40 +0300

    1. create indexStyle.css 2. create file js baseData.js for keepered data

--- socialPage/src/js/login_page.js
+++ socialPage/src/js/login_page.js
@@ -1,10 +1,11 @@
-document.getElementById('user_form').addEventListener('submit', function (e) {
+document.getElementById('userForm').addEventListener('submit', function (e) {
   e.preventDefault();
   let login = document.getElementById('login');
   let password = document.getElementById('password');
 
   console.log(login);
   console.log(password);
-  login.style.backgroundColor = login.value.match(/(^[a-z\d\-]+@[a-z\d\-]+\.[a-z\d\-]+$)|(^[a-z\d\-]+[a-z\d.\-]*[a-z\d-]+@[a-z\d-]+[a-z\d.\-]*\.[a-z\d]+$)/gi) ? '#008000' : '#F9D0C4';
+  login.style.backgroundColor = login.value.match(/[a-z]+/) ? '#008000' : '#F9D0C4';
+  password.style.backgroundColor = password.value.match(/\d+/) ? '#008000' : '#F9D0C4';
 
 });
