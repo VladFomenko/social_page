d911f22607ab351dc21b26f136cf3231e547a95d
VladFomenko <cheshirskiy.kot2311@gmail.com>
2021-06-09 11:06:56 +0300

    1. create indexStyle.css

--- socialPage/src/js/login_page.js
+++ socialPage/src/js/login_page.js
@@ -1,7 +1,10 @@
-document.getElementById('check').onclick = function () {
-  let login = document.getElementById('login').value;
-  let password = document.getElementById('password').value;
+document.getElementById('user_form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  let login = document.getElementById('login');
+  let password = document.getElementById('password');
 
   console.log(login);
   console.log(password);
-};
+  login.style.backgroundColor = login.value.match(/(^[a-z\d\-]+@[a-z\d\-]+\.[a-z\d\-]+$)|(^[a-z\d\-]+[a-z\d.\-]*[a-z\d-]+@[a-z\d-]+[a-z\d.\-]*\.[a-z\d]+$)/gi) ? '#008000' : '#F9D0C4';
+
+});
