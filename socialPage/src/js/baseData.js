de2bcb6e258599e0b50565b7ddf38dfe4c130d0e
VladFomenko <cheshirskiy.kot2311@gmail.com>
2021-06-11 14:56:43 +0300

    1. create indexStyle.css 2. create file js baseData.js for keepered data 3. failed to work through modules

---
+++ socialPage/src/js/baseData.js
@@ -0,0 +1,16 @@
+
+/*
+let userDataObject = {};
+let id000001 = {
+  id: "1",
+  login: "Ivan",
+  password: 123456,
+};
+let  userDataMassive = [];
+
+userDataMassive.push(id000001.id);
+//console.log(userDataMassive[0].login);
+console.log(userDataMassive);
+
+ */
+export let userData = [];
--- socialPage/src/js/login_page.js
+++ socialPage/src/js/login_page.js
@@ -1,11 +1,35 @@
+//import {userData} from './baseData.js';
+
 document.getElementById('userForm').addEventListener('submit', function (e) {
   e.preventDefault();
   let login = document.getElementById('login');
   let password = document.getElementById('password');
 
-  console.log(login);
-  console.log(password);
+
+  //проверка логина и пароля к требованиям
+  console.log(login.value);
+  console.log(password.value);
   login.style.backgroundColor = login.value.match(/[a-z]+/) ? '#008000' : '#F9D0C4';
   password.style.backgroundColor = password.value.match(/\d+/) ? '#008000' : '#F9D0C4';
+
+
+  //база днных пользователей
+  let userDataMassive = [0];
+
+  let idUser = userDataMassive.length;
+  let userData = {
+    id: idUser,
+    login: login.value,
+    password: password.value,
+  }
+
+  userDataMassive.push(userData);
+
+  console.log(userDataMassive);
+  console.log(userDataMassive[idUser]);
+
+  if (login.style.backgroundColor === 'rgb(0, 128, 0)' && password.style.backgroundColor === 'rgb(0, 128, 0)') {
+    window.location.href = './src/html/main_page.html';
+  }
 
 });
